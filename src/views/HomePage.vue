a844bc018ac358e0a6424f3cd2dbd3e3ec57deab
Fatima <fatim.torre@gmail.com>
2026-02-20 16:53:37 -0600

    add profile

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,7 +4,7 @@ import LinkCard from '@/components/LinkCard.vue'
 const profile = ref({
   name: 'Fatima Torres',
   slogan: 'STC IT Student',
-  avatar: ''
+  avatar: '8b97e421-39d2-4295-854f-d194e06a99fc.jpg'
   })
 </script>
 
@@ -12,15 +12,15 @@ const profile = ref({
   <main class="flex min-h-screen flex-col items-center justify-center px-4 py-8">
     <!-- Profile Header-->
      <div class="mb-8 flex flex-col items-center text-center">
-      <div class="h-24 w-24 overflow-hidden rounded-full border-green-400 shadow-lg shadow-green-500/20">
+      <div class="h-24 w-24 overflow-hidden rounded-full border-green-400 shadow-lg shadow-green-500/20 sm:h-32 sm:w-32">
       <img 
       :src="profile.avatar" 
       :alt="profile.name"
       class="h-fill w-full object-cover"
       />
       </div>
-        <h1 class="font-sans">{{  profile.name }}</h1>
-        <p>{{  profile.slogan }}</p>
+        <h1 class="font-sans mb-2 text-2xl font-bold text-white sm:text-3xl">{{  profile.name }}</h1>
+        <p class="max-w-xs text-gray-300 sm:text-lg">{{  profile.slogan }}</p>
      </div>
     
   </main>
